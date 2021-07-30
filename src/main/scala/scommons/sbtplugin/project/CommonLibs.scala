da2bc27991dccebe87c85a08fca19578fe07cfd9
Viktor Podzigun <viktor.podzigun@gmail.com>
2021-07-30 11:26:47 +0200

    Use cross-compiled play-json; defined joda-time dependency

--- src/main/scala/scommons/sbtplugin/project/CommonLibs.scala
+++ src/main/scala/scommons/sbtplugin/project/CommonLibs.scala
@@ -1,6 +1,7 @@
 package scommons.sbtplugin.project
 
 import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
+import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport.scalaJSVersion
 import sbt._
 
 trait CommonLibs {
@@ -8,8 +9,11 @@ trait CommonLibs {
   val akkaVersion = "2.6.14"
   val playVer = "2.8.8" // should be the same as Play sbt-plugin version (see plugins.sbt)
   val scaldiPlayVer = "0.6.1"
-  val playJsonVer = "2.8.1"
+  val playJsonVer: String =
+    if (scalaJSVersion.startsWith("0.6")) "2.8.1"
+    else "2.9.0"
   val playWsVer = "2.0.8"
+  val jodaTimeVer = "2.10.5"
 
   //////////////////////////////////////////////////////////////////////////////
   // jvm dependencies
@@ -19,6 +23,8 @@ trait CommonLibs {
   lazy val playJson = Def.setting("com.typesafe.play" %% "play-json" % playJsonVer)
   lazy val playWs = Def.setting("com.typesafe.play" %% "play-ahc-ws-standalone" % playWsVer)
 
+  lazy val jodaTime = Def.setting("joda-time" % "joda-time" % jodaTimeVer)
+  
   lazy val logback = Def.setting("ch.qos.logback" % "logback-classic" % "1.1.7")
   lazy val slf4jApi = Def.setting("org.slf4j" % "slf4j-api" % "1.7.12")
   lazy val jclOverSlf4j = Def.setting("org.slf4j" % "jcl-over-slf4j" % "1.7.12")
